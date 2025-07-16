17a82dc79ba39d8c353c59e95af913f9ba26ab85
Im Big Jet Plane <lamcz0407@gmail.com>
2025-07-16 14:00:27 +0700

    done project

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import axios from 'axios'
 
 import fetchWeatherToday from './api/WeatherTodayApi';
 import { TiWeatherCloudy } from "react-icons/ti";
+import { CiLocationArrow1 } from "react-icons/ci";
+import { CiTempHigh } from "react-icons/ci";
+import { IoWaterOutline } from "react-icons/io5";
+import { LuWind } from "react-icons/lu";
+
 
 
 function App() {
@@ -19,13 +24,13 @@ function App() {
 
 
 
+
   //fetch Weather today with location 
   useEffect(() => {
 
 
-      const fetchData = async () => {
-        try {
-          setLoading(true)
+    const fetchData = async () => {
+      try {
           setCurrentWeatherData(null)
           const data = await fetchWeatherToday(location);
           setWeatherData(data.forecast.forecastday);
@@ -35,6 +40,7 @@ function App() {
           console.error("Fetch weather error:", error);
         }
         finally{
+          setLoading(true)
           setLoading(false)
         }
       };
@@ -48,11 +54,11 @@ function App() {
   return (
     <> 
 
-    <div className='flex flex-col justify-center items-center p-5'>
+    <div className='flex flex-col justify-center items-center'>
 
         {/* Div display current  */}
 
-        <div className="relative w-300 h-200 rounded-lg p-6 text-white
+        <div className="relative w-full h-screen rounded-lg p-6 text-white
                 flex flex-col items-center
                 overflow-hidden
                 before:content-[''] 
@@ -62,7 +68,8 @@ function App() {
                 before:opacity-80
                 before:z-0 
                 bg-black">
-
+          
+          <p className='z-10 font-bold text-2xl'>ADAMO - WEATHER DATA</p>
           {/* Input field search location */}
           <div className='flex justify-center mt-5 w-full gap-3 z-10'>
               <input type="text" className='rounded-md w-100 h-10 border-2 border-white p-2 text-white placeholder-white focus:outline-none focus:ring-0 focus:border-white bg-transparent' placeholder='Search by name, zipcode.....' value={searchLocation} onChange={(e) => setSearchLocaion(e.target.value)} />
@@ -70,42 +77,60 @@ function App() {
           </div>
 
           <div className='bg-white w-250 h-0.5 z-10 mt-10 mb-5'></div>
-
+          <p className='z-10 font-bold text-lg'>Today</p>
           {/* Display current weather tod */}
-          <div className='z-10 flex w-250 h-50 rounded-lg items-center justify-between'>
+          <div className='z-10 flex w-250 h-50 rounded-lg items-center justify-between mb-5'>
               {/* Display status*/}
 
               {loading && (
-                <>
+                <>  
                   <p className='w-full items-center flex justify-center'>Loading...</p>
                 </>
               )}
 
               {currentWeatherData && (
-
                 <>
                     <TiWeatherCloudy className='w-40 h-40'></TiWeatherCloudy>
                     <div className='flex flex-col items-center justify-center'>
+                        <CiLocationArrow1 className='w-10 h-10 text-white mb-2'></CiLocationArrow1>
                         <p className='font-semibold text-2xl'>{detailLocation.name}</p>
                         <p className=''>{detailLocation.country}</p>
                     </div>
 
                     <div className='flex flex-col items-center justify-center'>
+                        <CiTempHigh className='w-10 h-10 text-white mb-2'></CiTempHigh>
                         <p className='font-semibold text-2xl'>{currentWeatherData.temp_c} °C</p>
                         <p className=''>Temperature</p>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
+                        <IoWaterOutline className='w-10 h-10 text-white mb-2'></IoWaterOutline>
                         <p className='font-semibold text-2xl'>{currentWeatherData.humidity} %</p>
                         <p className=''>Humidity</p>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
+                        <LuWind className='w-10 h-10 text-white mb-2'></LuWind>
                         <p className='font-semibold text-2xl'>{currentWeatherData.vis_km} km/h</p>
                         <p className=''>Wind speed</p>
                     </div>
                 </>
-               
-
               )}
+          </div>
+
+          <div className='bg-white w-250 h-0.5 z-10 mt-10 mb-5'></div>
+          
+
+          <p className='z-10 font-bold text-lg'>7 Days Forecast</p>
+          <div className='z-10 flex w-250 h-50 rounded-lg items-center justify-between p-2'>
+              
+              {weatherData && weatherData.map((weather: any) => (
+                <div className='bg-transparent flex flex-col justify-center items-center gap-2 border-1 rounded-lg p-2 backdrop-blur-xs'>
+                  <p className='font-semibold'>{weather.date.split("-").reverse().join("/")}</p>
+                  <TiWeatherCloudy className='w-10 h-10 text-white'></TiWeatherCloudy>
+                  <p>{weather.day.maxtemp_c}°C</p>
+                </div>
+
+              ))}
+              
           </div>
 
         </div>
